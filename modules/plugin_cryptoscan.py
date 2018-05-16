7f44fe6c491c97b60a639652758d39c0c587675c
bpottier <bpottier@funnelback.com>
2018-05-16 09:43:06 +0100

    Removed choice selection whilst signature scans are not enabled

--- modules/plugin_cryptoscan.py
+++ modules/plugin_cryptoscan.py
@@ -2,11 +2,13 @@ import binaryninja as bn
 from CryptoScan import CryptoScan
 
 def run_plugin(bv):
-    # For now this will reload configs on every run, might be desirable.
-    static_scan = bn.ChoiceField('Scan for constants', ['Yes', 'No'])
-    signature_scan = bn.ChoiceField('Scan IL signatures', ['Yes', 'No'])
-    bn.get_form_input([None, static_scan, None, signature_scan], 'Scanning options')
-    options = {'static' : static_scan.result == 0, 'signature' : signature_scan.result == 0}
+    # Just run everything until we have something tangible for IL signature detection
+    ## For now this will reload configs on every run, might be desirable.
+    ## static_scan = bn.ChoiceField('Scan for constants', ['Yes', 'No'])
+    ## signature_scan = bn.ChoiceField('Scan IL signatures', ['Yes', 'No'])
+    ## bn.get_form_input([None, static_scan, None, signature_scan], 'Scanning options')
+    ## options = {'static' : static_scan.result == 0, 'signature' : signature_scan.result == 0}
+    options = {'static' : True, 'signature' : False}
     if any(option for option in options.values()):
         cs = CryptoScan(bv, options)
         cs.start()
